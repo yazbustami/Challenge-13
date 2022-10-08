700e074259f8267f9b85e5d1b63e572073cd513b
Yasmin Bustami <yasminbustami@Yasmins-MacBook-Pro.local>
2022-10-07 21:20:37 -0500

    product.js done

--- models/Product.js
+++ models/Product.js
@@ -10,7 +10,44 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type:DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    price: {
+      type: DataTypes.DECIMAL(float),
+      allowNull: false,
+      validate: { 
+        isDecimal: true,
+      },
+    },
+
+    stock: {
+      type: DataTypes.INTEGER, 
+      allowNull: false,
+      defaultValue: 10,
+      validate: { 
+        isInteger: true,
+      },
+      },
+
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
   },
+
   {
     sequelize,
     timestamps: false,
