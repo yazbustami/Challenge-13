9ba35a23c7b8602641e8aa164e4f4631365c2ba0
Yasmin <bustamiyasmin@gmail.com>
2022-11-11 03:02:57 -0600

    tag-route, product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -2,17 +2,34 @@ const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
-
 // get all products
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product
+  .findAll({ include: [Category, Tag]})
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => { res.status(500).json(err);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+    Product
+    .findOne({ 
+      where: {id: req.params.id },
+      include: [Category, Tag]
+    })
+    .then(dbProductData => 
+      { if (!dbProductData) {
+        res.status(404).json ({ message: "NOT FOUND."});
+        return;
+      }
+      res.json(dbProductData)})
+    .catch(err => { res.status(500).json(err);
+    });
 });
 
 // create new product
@@ -91,6 +108,19 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product
+  .destroy({ 
+    where: {id: req.params.id }
+  })
+  .then(dbProductData => 
+    { if (!dbProductData) {
+      res.status(404).json ({ message: "NOT FOUND."});
+      return;
+    }
+    res.json({ message: "NOT FOUND"});
+  })
+  .catch(err => { res.status(500).json(err);
+  });
 });
 
 module.exports = router;
